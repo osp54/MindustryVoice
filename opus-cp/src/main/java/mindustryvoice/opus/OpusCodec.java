75f9b3486b259913f82296e68289f63da089fb1e
osp54 <clashmekers54@gmail.com>
2023-07-11 12:24:45 +0300

    fix

--- opus-cp/src/main/java/mindustryvoice/opus/OpusCodec.java
+++ opus-cp/src/main/java/mindustryvoice/opus/OpusCodec.java
@@ -1,19 +1,34 @@
 package mindustryvoice.opus;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.AccessDeniedException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.NoSuchElementException;
+import java.util.Arrays;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.TargetDataLine;
 
 public class OpusCodec {
+    private static OpusCodec INSTANCE;
+
+    public static OpusCodec instance() {
+        if (INSTANCE == null)
+            return INSTANCE = new OpusCodec();
+        return INSTANCE;
+    }
+
+    public final int frameSize = 960;
+    public final int sampleRate = 48000;
+    public final int channels = 1;
+    public final int bitrate = 64000;
+    public final int maxFrameSize = 6 * 960;
+    public final int maxPacketSize = 3 * 1276;
+
     private final OpusCodecOptions opusOptions = new OpusCodecOptions();
 
     private OpusCodec() {
-
+        System.loadLibrary("opus-cp");
     }
 
     public int getFrameSize() {
@@ -53,23 +68,7 @@ public class OpusCodec {
         return this.encodeFrame(bytes, 0, bytes.length);
     }
 
-    /**
-     * Encodes a chunk of raw PCM data.
-     *
-     * @param bytes data to encode. Must have a length of CHANNELS * FRAMESIZE * 2.
-     * @return encoded data
-     *         <p>
-     *         throws {@link IllegalArgumentException} if length is invalid
-     */
-    public byte[] encodeFrame(byte[] bytes, int offset, int length) {
-        if (length != this.getChannels() * this.getFrameSize() * 2)
-            throw new IllegalArgumentException(
-                    String.format("data length must be == CHANNELS * FRAMESIZE * 2 (%d bytes) but is %d bytes",
-                            this.getChannels() * this.getFrameSize() * 2, bytes.length));
-        return this.encodeFrame(bytes, offset, length);
-    }
-
-    private native byte[] encodeFrame(long encoder, byte[] in, int offset, int length);
+    private native byte[] encodeFrame(byte[] in, int offset, int length);
 
     /**
      * Decodes a chunk of opus encoded pcm data.
@@ -80,7 +79,31 @@ public class OpusCodec {
      */
     private native byte[] decodeFrame(byte[] out);
 
-    private native void destroyEncoder();
+    public static void main(String[] args) throws LineUnavailableException, IOException {
+        OpusCodec codec = OpusCodec.instance();
 
-    private native void destroyDecoder();
+        AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 48000, 16, 1, 2, 48000, false);
+
+        TargetDataLine microphone = AudioSystem.getTargetDataLine(format);
+        microphone.open(microphone.getFormat());
+        microphone.start();
+
+        // get default speaker and open it at the selected format
+        SourceDataLine speaker = AudioSystem.getSourceDataLine(format);
+        speaker.open(microphone.getFormat());
+        speaker.start();
+
+        while (true) {
+            // Reading microphone data
+            byte[] data = new byte[codec.getChannels() * codec.getFrameSize() * 2];
+            microphone.read(data, 0, data.length);
+
+            // Encoding PCM data chunk
+            byte[] encode = codec.encodeFrame(data);
+            //System.out.println(Arrays.toString(encode));
+            // Decoding PCM data chunk
+            byte[] decoded = codec.decodeFrame(encode);
+            speaker.write(decoded, 0, decoded.length);
+        }
+    }
 }
